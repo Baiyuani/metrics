e8d477bd6b37dc4530c8191959726500da5b55d6
Tracy <7384188+hanab1@user.noreply.gitee.com>
2022-05-19 10:22:45 +0800

    update metrics

--- src/main.py
+++ src/main.py
@@ -4,19 +4,13 @@ import requests
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 }
-
-
-"""python内置变量__name__的值是字符串__main__ 。Flask类将这个参数作为程序名称。当然这个是可以自定义的，比如app = Flask("my-app")。"""
 app = Flask(__name__)
-"""Flask默认使用static目录存放静态资源，templates目录存放模板，这是可以通过设置参数更改的：
-app = Flask("my-app", static_folder="path1", template_folder="path2")
-"""
 
 
 def get_status(url):
     try:
-        response = requests.get(url, headers=headers)  # 生成一个response对象
-        return str(response.status_code)  # 状态码
+        response = requests.get(url, headers=headers)
+        return str(response.status_code)
     except:
         pass
 
@@ -26,7 +20,7 @@ def config():
         dic = []
         for line in file.readlines():
             line = line.strip('\n')
-            if line == '':  # 去除空行
+            if line == '':
                 continue
             b = line.split(',')
             dic.append(b)
@@ -38,7 +32,7 @@ def metrics(response=''):
     for name, url in config().items():
         code = get_status(url)
         try:
-            r = 'ketanyun_' + name + '_status{origin="ketanyun"}' + ' ' + code + '\n'
+            r = 'ketanyun_' + name + '_status{origin="ketanyun",svc=' + name + ',url=' + url + '}' + ' ' + code + '\n'
         except:
             pass
         response += r
@@ -47,5 +41,4 @@ def metrics(response=''):
 
 
 if __name__ == '__main__':
-    # create_app().run(host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0', port=5000, debug=True)  # 调试模式
+    app.run(host='0.0.0.0', port=5000, debug=True)
